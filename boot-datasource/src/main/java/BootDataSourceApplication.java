b961de2cc69f70f8088ed7bf5926fb4d6c635d5d
chenfangchao <socrates11@foxmail.com>
2022-04-02 11:19:21 +0800

    [init]初始化boot项目

---
+++ boot-datasource/src/main/java/BootDataSourceApplication.java
@@ -0,0 +1,9 @@
+/**
+ * @author chenfangchao
+ * @title: BootDataSourceApplication
+ * @projectName boot-project
+ * @description: TODO
+ * @date 2022/4/2 11:13 AM
+ */
+public class BootDataSourceApplication {
+}
