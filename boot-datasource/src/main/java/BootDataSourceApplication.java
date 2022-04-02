919ae975e4b94cd7aa39f34e5f015f6b7655d518
chenfangchao <socrates11@foxmail.com>
2022-04-02 11:40:26 +0800

    新增子项目

--- boot-datasource/src/main/java/BootDataSourceApplication.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author chenfangchao
- * @title: BootDataSourceApplication
- * @projectName boot-project
- * @description: TODO
- * @date 2022/4/2 11:13 AM
- */
-public class BootDataSourceApplication {
-}
---
+++ boot-datasource/src/main/java/cn/com/wudskq/BootDataSourceApplication.java
@@ -0,0 +1,18 @@
+package cn.com.wudskq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenfangchao
+ * @title: cn.com.wudskq.BootDataSourceApplication
+ * @projectName boot-project
+ * @description: TODO boot配置多数据源
+ * @date 2022/4/2 11:13 AM
+ */
+@SpringBootApplication
+public class BootDataSourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootDataSourceApplication.class,args);
+    }
+}
