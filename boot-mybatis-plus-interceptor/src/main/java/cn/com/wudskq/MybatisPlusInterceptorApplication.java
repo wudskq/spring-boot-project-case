c2979e30b112399902e46ee6a3334c32cfb3cf0a
chenfangchao <socrates11@foxmail.com>
2022-04-07 02:50:16 +0800

    [add]#boot集成mp

---
+++ boot-mybatis-plus-interceptor/src/main/java/cn/com/wudskq/MybatisPlusInterceptorApplication.java
@@ -0,0 +1,18 @@
+package cn.com.wudskq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenfangchao
+ * @title: MybatisPlusInterceptorApplication
+ * @projectName boot-project
+ * @description: TODO boot集成mybatis-plus
+ * @date 2022/4/6 2:38 PM
+ */
+@SpringBootApplication
+public class MybatisPlusInterceptorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusInterceptorApplication.class,args);
+    }
+}
