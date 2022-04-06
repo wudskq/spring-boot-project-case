c354375c56f8a1028583a0da14be536cd880ccf6
chenfangchao <socrates11@foxmail.com>
2022-04-07 05:54:12 +0800

    [add]#新增boot集成多个kafka配置(acl权限验证&&批量拉取)

--- boot-kafka/src/main/java/cn/com/wudskq/consumer/Consumer.java
+++ boot-kafka/src/main/java/cn/com/wudskq/consumer/Consumer.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author chenfangchao
  * @title: Consumer
@@ -42,6 +45,18 @@ public class Consumer {
             containerFactory = "kafkaOneContainerFactory")
     public void listenerOne(ConsumerRecord<?, ?> record) {
         logger.info("第一个消费者收到 "+topic1+ "的消息 "+record.value());
+    }
+
+
+    //配置的第二个接入的消费者
+    //设置批量拉取poll
+    @KafkaListener(topics = "${wudskq.kafka.topic.test2}", groupId = "${wudskq.kafka.topic.group}",
+            containerFactory = "kafkaTwoContainerFactory")
+    public void listenerTwo(List<ConsumerRecord<?, ?>> record) {
+        record.forEach(consumerRecord -> {
+            logger.info("第二个消费者收到 "+topic2+ "的消息 "+consumerRecord.value());
+        });
+
     }
 
 }
